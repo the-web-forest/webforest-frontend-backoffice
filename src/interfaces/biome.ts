341408a72dc127b1e680788c35a395713a4f3b2f
viniciuscasarin <casarinvinicius@gmail.com>
2024-07-01 19:48:19 -0300

    feat: create biome form

---
+++ src/interfaces/biome.ts
@@ -0,0 +1,3 @@
+export default interface Biome {
+  name: string;
+}
---
+++ src/pages/Biome/List/List.tsx
@@ -0,0 +1,15 @@
+import Dashboard from "@/components/templates/Dashboard";
+
+import { NAV_LINKS } from "@/constants/nav";
+
+export default function List() {
+  return (
+    <Dashboard.Root>
+      <Dashboard.Sidebar navLinks={NAV_LINKS} />
+      <Dashboard.Title>Ver biomas</Dashboard.Title>
+      <Dashboard.Main>
+        <h1>Conteúdo Principal</h1>
+      </Dashboard.Main>
+    </Dashboard.Root>
+  );
+}
